062a058a993c080f0be67dc07ec5478505dcd0b7
shortnd <ococncol@gmail.com>
2018-10-26 18:35:28 -0400

    beginning threads test

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Fourm Threads</div>
+
+                <div class="card-body">
+                    @foreach ($threads as $thread)
+                        <h4>
+                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                        </h4>
+                        <div class="body">
+                            {{ $thread->body }}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
